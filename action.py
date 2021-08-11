8ba8a7ef2135cbe6f97d1137e4bac844cd06a87d
Harry Berg <harryaberg@gmail.com>
2021-08-11 11:08:18 +0100

    Creates form if it doesnt exist or updates it otherwise

--- action.py
+++ action.py
@@ -14,16 +14,39 @@ with open(quiz_file) as f:
 tf = Typeform(token=token)
 forms = tf.forms
 
+# GET ENTRIES OF EXISTING QUIZZES # TODO convert to actual SQL db
+local_store_fp = "local_quiz_db_store.yaml"
+with open(local_store_fp) as f:
+    quiz_ids = yaml.safe_load(f)
+print("quizzes", quiz_ids)
+if not quiz_ids:
+    quiz_ids = {}
+
 # CHECK IF FORM EXISTS
 exists = True
 try:
-    forms.get(quiz["id"])
+    typeform_quiz_id = quiz_ids[quiz["id"]]  # check it exists in local storage
+    forms.get(typeform_quiz_id)  # double check it exists in typeform
+except KeyError:
+    print("Form not in local db")
+    exists = False
 except:
+    print("Form not in typeform")
     exists = False
 print("Exists?", exists)
 
-quiz.pop("id")
+local_quiz_id = quiz.pop("id")
+
 # CREATE FORM IF IT DOESNT EXIST
-forms.create(quiz)
+if not exists:
+    print("Creating", local_quiz_id)
+    response = forms.create(quiz)
+    typeform_quiz_id = response["id"]
+    quiz_ids[local_quiz_id] = typeform_quiz_id
+    with open(local_store_fp, "w") as f:
+        yaml.dump(quiz_ids, f)
 
 # UPDATE FORM IF IT EXISTS
+else:
+    print("Updating", local_quiz_id)
+    response = forms.update(typeform_quiz_id, quiz)
