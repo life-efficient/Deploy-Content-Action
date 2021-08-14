e9a82d352c54b93a5cdf445daebcd03258273e95
Harry Berg <harryaberg@gmail.com>
2021-08-14 11:04:03 +0100

    Removed duplication of API root

--- action.py
+++ action.py
@@ -1,83 +1,63 @@
 import yaml
-from typeform import Typeform
+from utils import (
+    get_lesson_paths_in_module,
+    get_meta,
+    get_module_paths,
+    get_quiz_path_in_lesson,
+)
+import os
+import requests
+import json
 
-# GET CREDENTIALS
-with open("creds.json") as f:
-    token = yaml.safe_load(f)["typeform_token"]
-
-# GET QUIZ YAML
-quiz_file = "example_unit/0. First Module/0. First Module's First Lesson/.quiz.yaml"
-with open(quiz_file) as f:
-    quiz = yaml.safe_load(f)
-
-# INITIALISE TYPEFORM CLIENT
-tf = Typeform(token=token)
-forms = tf.forms
+API_ROOT = "http://localhost:8000"
 
 
-def format_quiz(quiz):
-    """Takes in our quiz format and converts it to that required by typeform"""
-    with open("quiz_template.yaml") as f:
-        template = yaml.safe_load(f)
-    template["title"] = quiz["name"]
-    template["fields"] = [
-        {
-            "ref": f"question-{q_idx}",
-            "title": q["question"],
-            "type": "multiple_choice",
-            "properties": {
-                "allow_multiple_selection": True,
-                "choices": [
-                    {"ref": f"option-{op_idx}", "label": option["option"]}
-                    for op_idx, option in enumerate(q["options"])
-                ],
-            },
-        }
-        for q_idx, q in enumerate(quiz["questions"])
-    ]
-    return template
+class Client:
+    def create_or_update_unit(self, unit_meta):
+        self._request(f"{API_ROOT}/content/unit", unit_meta)
 
+    def create_or_update_module(self, module_meta):
+        self._request(f"{API_ROOT}/content/module", module_meta)
 
-def create_or_update_quiz(quiz):
+    def create_or_update_lesson(self, lesson_meta):
+        self._request(f"{API_ROOT}/content/lesson", lesson_meta)
 
-    # GET ENTRIES OF EXISTING QUIZZES # TODO convert to actual SQL db
-    local_store_fp = "local_quiz_db_store.yaml"
-    with open(local_store_fp) as f:
-        quiz_ids = yaml.safe_load(f)
-    print("quizzes", quiz_ids)
-    if not quiz_ids:
-        quiz_ids = {}
+    def create_or_update_quiz(self, quiz):
+        self._request(f"{API_ROOT}/content/quiz", quiz)
 
-    # CHECK IF FORM EXISTS
-    try:
-        typeform_quiz_id = quiz_ids[quiz["id"]]  # check it exists in local storage
-        forms.get(typeform_quiz_id)  # double check it exists in typeform
-        exists = True
-    except KeyError:
-        print("Form not in local db")
-        exists = False
-    except:
-        print("Form not in typeform")
-        exists = False
-    print("Exists?", exists)
-
-    local_quiz_id = quiz.pop("id")
-
-    # CREATE FORM IF IT DOESNT EXIST
-    if not exists:
-        print("Creating", local_quiz_id)
-        response = forms.create(format_quiz(quiz))
-        typeform_quiz_id = response["id"]
-        quiz_ids[local_quiz_id] = typeform_quiz_id
-        with open(local_store_fp, "w") as f:
-            yaml.dump(quiz_ids, f)
-
-    # UPDATE FORM IF IT EXISTS
-    else:
-        print("Updating", local_quiz_id)
-        # typeform_quiz_id = get_typeform_quiz_id(quiz["id"])
-        response = forms.update(typeform_quiz_id, format_quiz(quiz))
+    def _request(self, url, payload_yaml):
+        response = requests.post(url, data=json.dumps(payload_yaml))
+        print(response)
 
 
 if __name__ == "__main__":
-    create_or_update_quiz(quiz)
+
+    client = Client()
+
+    # CREAT UNIT ENTRY
+    unit_meta = get_meta(".unit.yaml")
+    unit_meta["name"] = os.path.dirname(os.path.realpath(__file__)).split("/")[-1]
+    client.create_or_update_unit(unit_meta)
+
+    # CREATE MODULE ENTRIES
+    for module_path in get_module_paths():
+        module_meta = get_meta(os.path.join(module_path, ".module.yaml"))
+        module_meta["name"] = module_path.split("/")[-1]
+        module_meta["unit_id"] = unit_meta["id"]
+        client.create_or_update_module(module_meta)
+
+        lesson_paths = get_lesson_paths_in_module(module_path)
+
+        # CREATE LESSON ENTRIES
+        for lesson_path in lesson_paths:
+            lesson_meta = get_meta(os.path.join(lesson_path, ".lesson.yaml"))
+            lesson_meta["name"] = lesson_path.split("/")[-1]
+            lesson_meta["module_id"] = module_meta["id"]
+            client.create_or_update_lesson(lesson_meta)
+
+            # CREATE QUIZ ENTRY
+            quiz_path = get_quiz_path_in_lesson(lesson_path)
+            with open(quiz_path) as f:
+                quiz = yaml.safe_load(f)
+            quiz["lesson_id"] = lesson_meta["id"]
+            client.create_or_update_quiz(quiz)
--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 import os
+import yaml
+
+
+def get_meta(filepath):
+    with open(filepath) as f:
+        meta = yaml.safe_load(f)
+    return meta
 
 
 def get_module_paths():
@@ -8,15 +15,32 @@ def get_module_paths():
 def get_lesson_paths():
     paths = []
     modules = get_module_paths()
-    for module in modules:
-        lesson_names = [m for m in os.listdir(module)]
-        lesson_names = [
-            ln for ln in lesson_names if os.path.isdir(os.path.join(module, ln))
-        ]
-
-        for lesson in lesson_names:
-            if lesson == "Extra":
-                continue
-            path = os.path.join(module, lesson)
-            paths.append(path)
+    for module_path in modules:
+        lesson_paths = get_lesson_paths_in_module(module_path)
+        paths.extend(lesson_paths)
     return paths
+
+
+def get_lesson_paths_in_module(module_path):
+    lesson_names = [m for m in os.listdir(module_path)]
+    lesson_names = [
+        ln for ln in lesson_names if os.path.isdir(os.path.join(module_path, ln))
+    ]
+
+    lesson_paths = []
+    for lesson in lesson_names:
+        if lesson == "Extra":
+            continue
+        path = os.path.join(module_path, lesson)
+        lesson_paths.append(path)
+    return lesson_paths
+
+
+def get_quiz_path_in_lesson(lesson_path):
+    return os.path.join(lesson_path, ".quiz.yaml")
+
+
+def get_quiz_paths():
+    lesson_folder_paths = get_lesson_paths()
+    for lesson_folder_path in lesson_folder_paths:
+        get_quiz_path_in_lesson(lesson_folder_path)
